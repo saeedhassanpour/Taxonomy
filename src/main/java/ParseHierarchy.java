51fdda6a5fdd79b6f6b09bd9fb139da72f0d7810
Saeed Hassanpour <saeedhp@gmail.com>
2014-09-23 16:06:42 -0700

    Clean up the code

--- src/main/java/ParseHierarchy.java
+++ src/main/java/ParseHierarchy.java
@@ -10,6 +10,7 @@ import org.semanticweb.owlapi.model.IRI;
 import org.semanticweb.owlapi.model.OWLClass;
 import org.semanticweb.owlapi.model.OWLDataFactory;
 import org.semanticweb.owlapi.model.OWLDataProperty;
+import org.semanticweb.owlapi.model.OWLIndividual;
 import org.semanticweb.owlapi.model.OWLLiteral;
 import org.semanticweb.owlapi.model.OWLNamedIndividual;
 import org.semanticweb.owlapi.model.OWLOntology;
@@ -50,8 +51,6 @@ public class ParseHierarchy {
 	    OWLDataProperty prefNameString = df.getOWLDataProperty(IRI.create("http://www.owl-ontologies.com/RADLEX.owl#Preferred_name"));
 	    OWLReasoner reasoner = reasonerFactory.createReasoner(o);
 	    
-
-	    
 //		http://www.owl-ontologies.com/RADLEX.owl#Preferred_name
 //    	http://www.owl-ontologies.com/RADLEX.owl#Synonym
 //	    http://www.owl-ontologies.com/RADLEX.owl#Misspelling_of_term
@@ -88,92 +87,64 @@ public class ParseHierarchy {
 	    	dVStrings.add(df.getOWLDataProperty(IRI.create(dv)));
 	    }
 	    
-		//pathophysiologic finding, benign finding,portion of body substance, object, imaging observation
+		//pathophysiologic finding, benign finding, portion of body substance, object, imaging observation
 		//Radlex descriptor
 		//anatomical structure, immaterial anatomical entity, anatomical set
+	    //size descriptor
 	    //certainty descriptor
-	    //orientation descriptor
-	    //modality descriptor
+	    //orientation descriptor, modality descriptor
 	    //imaging modality
-	    String root = "imaging modality";
-	    
-	    for (OWLNamedIndividual i : o.getIndividualsInSignature()) {
-	        for (OWLLiteral lit : EntitySearcher.getDataPropertyValues(i, prefNameString, o)) {
-	        	
-	        	if(normalize(renderer.render(lit)).equals(root)){
+		String root = "imaging modality";
 
-	        		printNames(i, dVStrings, o, renderer, dictionary);
-	        		
-	        		OWLClass cls = df.getOWLClass(i.getIRI());
-	        		NodeSet<OWLClass> clss = reasoner.getSubClasses(cls, false);
-	        		
-	        		for (Node<OWLClass> subcls : clss)
-	        		{
-	        			OWLNamedIndividual si = df.getOWLNamedIndividual(subcls.getRepresentativeElement().getIRI());
-		        		printNames(si, dVStrings, o, renderer, dictionary);
-	        		}
-	        	}
-	        	
-	        }
-	    }
-	    
-	    
-    	PrintWriter pw = new PrintWriter(root+".txt","UTF-8"); 
-	    for(String e: dictionary)
-	    {
-	    	pw.println(e);
-	    }
-	    pw.close();
+		for (OWLNamedIndividual i : o.getIndividualsInSignature()) {
+			for (OWLLiteral lit : EntitySearcher.getDataPropertyValues(i, prefNameString, o)) {
 
-		
-//		 for (OWLClass cls : o.getClassesInSignature())
-//		 {
-//		 System.out.println(cls);
-//		 }
-		
-//		for (OWLDataProperty dp : o.getDataPropertiesInSignature())
-//		{
-//			System.out.println(dp);
-//		}
-		
-//		for (OWLNamedIndividual ni : o.getIndividualsInSignature())
-//		{
-//			System.out.println(ni);
-////			for(ni.get)
-////			{
-////				System.out.println(dp);
-////			}
-//		}
-		
-//		for (OWLClass cls : o.getClassesInSignature()) {
-//			for (OWLIndividual indv : cls.getIndividualsInSignature()) {
-//				System.out.println(indv);
-////				for(OWLDataProperty dv :indv.getDataPropertiesInSignature())
-////				{
-////					System.out.println(dv);
-////				}
-//			}
-//		}
-		
+				if (normalize(renderer.render(lit)).equals(root)) 
+				{
+					printNames(i, dVStrings, o, renderer, dictionary);
 
-		// reasoner from previous example...
-		// for each class, look up the instances
-//		for (OWLClass c : o.getClassesInSignature()) {
-//			// the boolean argument specifies direct subclasses
-//			for (OWLNamedIndividual i : r.getInstances(c, true).getFlattened()) {
-//				System.out.println(labelFor(i, o) + ":" + labelFor(c, o));
-//				// look up all property assertions
-//				for (OWLObjectProperty op : o.getObjectPropertiesInSignature()) {
-//					NodeSet<OWLNamedIndividual> petValuesNodeSet = r
-//							.getObjectPropertyValues(i, op);
-//					for (OWLNamedIndividual value : petValuesNodeSet
-//							.getFlattened())
-//						System.out.println(labelFor(i, o) + " "
-//								+ labelFor(op, o) + " " + labelFor(value, o));
-//				}
-//			}
-//		}
+					OWLClass cls = df.getOWLClass(i.getIRI());
+					NodeSet<OWLClass> clss = reasoner.getSubClasses(cls, false);
 
+					for (Node<OWLClass> subcls : clss) 
+					{
+						OWLNamedIndividual si = df.getOWLNamedIndividual(subcls.getRepresentativeElement().getIRI());
+						printNames(si, dVStrings, o, renderer, dictionary);
+					}
+				}
+
+			}
+		}
+
+		PrintWriter pw = new PrintWriter(root + ".txt", "UTF-8");
+		for (String e : dictionary) {
+			pw.println(e);
+		}
+		pw.close();
+	}
+	
+	public void helperMethod(OWLOntology o) {
+		for (OWLClass cls : o.getClassesInSignature()) {
+			System.out.println(cls);
+		}
+
+		for (OWLDataProperty dp : o.getDataPropertiesInSignature()) {
+			System.out.println(dp);
+		}
+
+		for (OWLNamedIndividual ni : o.getIndividualsInSignature()) {
+			System.out.println(ni);
+
+		}
+
+		for (OWLClass cls : o.getClassesInSignature()) {
+			for (OWLIndividual indv : cls.getIndividualsInSignature()) {
+				System.out.println(indv);
+				for (OWLDataProperty dv : indv.getDataPropertiesInSignature()) {
+					System.out.println(dv);
+				}
+			}
+		}
 	}
 
 }
