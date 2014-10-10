17eca0d0e9d4db6e27c90c51b5bbf127f3f81095
sh-repos <saeedhp@gmail.com>
2014-10-10 12:25:48 -0700

    Minor formating

--- src/main/java/ParseHierarchytoGazette.java
+++ src/main/java/ParseHierarchytoGazette.java
@@ -10,7 +10,6 @@ import org.semanticweb.owlapi.model.IRI;
 import org.semanticweb.owlapi.model.OWLClass;
 import org.semanticweb.owlapi.model.OWLDataFactory;
 import org.semanticweb.owlapi.model.OWLDataProperty;
-import org.semanticweb.owlapi.model.OWLIndividual;
 import org.semanticweb.owlapi.model.OWLLiteral;
 import org.semanticweb.owlapi.model.OWLNamedIndividual;
 import org.semanticweb.owlapi.model.OWLOntology;
